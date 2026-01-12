d01582b055c88749eaef34b12f3802a5f727f218
MrMugwump <bbridenb@macalester.edu>
2026-01-12 16:52:18 -0600

    Added number of enrolled students to teaching assignements description

--- createCalFile.py
+++ createCalFile.py
@@ -181,6 +181,9 @@ for i in range(0,numRows):
     if response == 1:
         instructor = workdayCSV.loc[i,"Instructor"]
         description = description + "\\n" + instructor #\\n to have a line break in the description but not in the .ics file.
+    else:
+        numOfStudents = workdayCSV.loc[i,"Number of Enrolled Students"]
+        description = description + "\\n Number of students: "+str(numOfStudents)
     f.write("DESCRIPTION:"+ description+"\n")
 
     f.write("END:VEVENT\n")
